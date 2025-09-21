c3484207b8143481069a70c11de778280f9a113e
Marcos <marcosles7485@gmail.com>
2025-09-21 20:50:16 -0300

    fixes

--- resources/components/FooterView.vue
+++ resources/components/FooterView.vue
@@ -10,9 +10,9 @@ const languageStore = useLanguageStore();
     <a href="https://wa.me/541136190245?text=¡Hola!%20Quisiera%20más%20información" class="whatsapp-float"
       target="_blank">
 
-      <span class="whatsapp-text"  v-if="languageStore.languages === 'esp' || languageStore.languages.length === 0"><i class="fab fa-whatsapp whatsapp-icon"></i> Consulte!</span>
-      <span class="whatsapp-text"  v-if="languageStore.languages === 'pt'"><i class="fab fa-whatsapp whatsapp-icon"></i> Fale Conosco!</span>
-      <span class="whatsapp-text"  v-if="languageStore.languages === 'eng'"><i class="fab fa-whatsapp whatsapp-icon"></i> Contact Us!</span>
+      <span class="whatsapp-text"  v-if="languageStore.languages === 'esp' || languageStore.languages.length === 0"><i class="fab fa-whatsapp whatsapp-icon"></i> Contacto</span>
+      <span class="whatsapp-text"  v-if="languageStore.languages === 'pt'"><i class="fab fa-whatsapp whatsapp-icon"></i> Contato</span>
+      <span class="whatsapp-text"  v-if="languageStore.languages === 'eng'"><i class="fab fa-whatsapp whatsapp-icon"></i> Contact</span>
     </a>
   </div>
 </template>
